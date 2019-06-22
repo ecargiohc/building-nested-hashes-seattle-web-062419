0ea0ba3f8d9663af79c0b3bdc640390967ca9bea
Grace Colette <ilovegrace09@gmail.com>
2019-06-22 02:41:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,9 @@ def second_challenge
 
       #your key/value pairs here!
       }, 
-    :capulet => {
+    :capulet => {:patriarch => {}, :matriarch => {},
+:heroine => {}, :heroine_friends => {} 
+    
       #your key/value pairs here!
       }}
 
