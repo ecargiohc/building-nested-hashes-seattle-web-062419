79c4092fde65acf675c750d57decfe68925cedda
Grace Colette <ilovegrace09@gmail.com>
2019-06-22 02:41:26 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,7 @@ def second_challenge
       #your key/value pairs here!
       }, 
     :capulet => {:patriarch => {}, :matriarch => {},
-:heroine => {}, :heroine_friends => {} 
+:heroine => {}, :heroine_friends => []
     
       #your key/value pairs here!
       }}
