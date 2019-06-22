6d512f8dc92fe090afaa9d7903107e0de5759928
Grace Colette <ilovegrace09@gmail.com>
2019-06-22 02:41:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {:patriarch => {}, :matriarch => {},
-:hero => {}, :hero_friends => {[]} 
+:hero => {}, :hero_friends => [] 
 
       #your key/value pairs here!
       }, 
